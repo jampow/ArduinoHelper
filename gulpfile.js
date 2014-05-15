e5dc0a2d2d59774ab75594be5d08e8b05b2c0690
Gianpaulo M. Soares <jam_pow@hotmail.com>
2014-05-14 21:12:59 -0300

    iniciando projeto com gulp \o/

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require( 'gulp' );
+
+var EXPRESS_PORT = 8000,
+	EXPRESS_ROOT = __dirname,
+	LIVERELOAD_PORT = 35729,
+	lr;
+
+function startExpress(){
+	var express = require( 'express' ),
+		app = express();
+
+	app.use( require( 'connect-livereload' )() );
+	app.use( express.static( EXPRESS_ROOT ) );
+	app.listen( EXPRESS_PORT );
+}
+
+function startLivereload(){
+	lr = require( 'tiny-lr' )();
+	lr.listen( LIVERELOAD_PORT );
+}
+
+function notifyLivereload( event ){
+	var fileName = require( 'path' ).relative( EXPRESS_ROOT, event.path );
+	lr.changed({
+		body: {
+			files: [ fileName ]
+		}
+	});
+}
+
+gulp.task( 'default', function(){
+	startExpress();
+	startLivereload();
+	gulp.watch( '*.html', notifyLivereload );
+});
